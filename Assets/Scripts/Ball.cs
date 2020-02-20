5195b473f56b9ced0170d842457b7ec0a04dc86e
Blake <telefragent@gmail.com>
2020-02-20 14:09:33 -0500

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+	public int TargetSceneIndex;
+	public Rigidbody2D Rb;
+	public float MoveForce = 2f;
+
+	private void FixedUpdate()
+	{
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			Rb.AddForce(new Vector2(-MoveForce, 0f));
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag("Wall"))
+		{
+			GetComponent<SpriteRenderer>().color = Color.red;
+		}
+		else if(collision.gameObject.CompareTag("Goal"))
+		{
+			SceneManager.LoadScene(TargetSceneIndex);
+		}
+	}
+}
